07f7f41b0dc16cce4e47a8f9a3328911e411912d
lichong0309 <chongli0309@gmail.com>
2021-09-06 12:27:54 +0900

    update

--- testhan/han/model_hetero.py
+++ testhan/han/model_hetero.py
@@ -13,6 +13,7 @@ import torch.nn.functional as F
 
 import dgl
 from dgl.nn.pytorch import GATConv
+import time
 
 class SemanticAttention(nn.Module):
     def __init__(self, in_size, hidden_size=128):
@@ -58,13 +59,17 @@ class HANLayer(nn.Module):
     def __init__(self, meta_paths, in_size, out_size, layer_num_heads, dropout):
         super(HANLayer, self).__init__()
 
+        inter_node_time = 0.0
         # One GAT layer for each meta path based adjacency matrix
         self.gat_layers = nn.ModuleList()
         for i in range(len(meta_paths)):
+            temp_time_start = time.time()
             self.gat_layers.append(GATConv(in_size, out_size, layer_num_heads,
                                            dropout, dropout, activation=F.elu,
                                            allow_zero_in_degree=True))
-            print("test")
+            temp_time_fin = time.time()
+            inter_node_time = inter_node_time + temp_time_fin - temp_time_start
+        print("inter_node_time:", inter_node_time)
         self.semantic_attention = SemanticAttention(in_size=out_size * layer_num_heads)
         self.meta_paths = list(tuple(meta_path) for meta_path in meta_paths)
 
